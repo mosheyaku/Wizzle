776b2be9bc0cfc87185dd49977c9f0a2a6d11707
mosheyaku <mosheyaku@gmail.com>
2025-07-10 13:46:34 +0300

    Add pdf upload api and react upload component

---
+++ backend/pdfs/views.py
@@ -0,0 +1,15 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import status
+from .serializers import UploadedPDFSerializer
+
+class PDFUploadView(APIView):
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UploadedPDFSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
