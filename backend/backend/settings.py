77a6374fed70afb508ad01b907424bc135be3e14
mosheyaku <mosheyaku@gmail.com>
2025-07-16 00:15:00 +0300

    add save word button and fix word saving by syncing tokens, auto logout on mismatch, and refreshing accessToken

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 from decouple import config
 
@@ -148,4 +149,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),  
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),    
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
