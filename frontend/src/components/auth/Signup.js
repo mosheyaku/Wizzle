4b895cfb5586a860b9acc276eab107d26b1d40e3
mosheyaku <mosheyaku@gmail.com>
2025-07-16 21:13:07 +0300

    diployment settings

--- frontend/src/components/auth/Signup.js
+++ frontend/src/components/auth/Signup.js
@@ -43,7 +43,7 @@ export default function Signup({ onSignupSuccess }) {
     try {
       console.log("BASE_URL:", process.env.REACT_APP_API_BASE_URL)
       const res = await axios.post(
-        `https://${process.env.REACT_APP_API_BASE_URL}/api/users/register/`,
+        `${process.env.REACT_APP_API_BASE_URL}/api/users/register/`,
         submitData
       );
 
